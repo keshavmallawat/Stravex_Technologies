b9854924070bdf9c2be421012a7386117bf88f67
keshavmallawat <mallawatkeshav@gmail.com>
2025-10-14 16:04:47 +0530

    chore: inline sourcemaps for prod debugging

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
   build: {
-    sourcemap: true,
+    sourcemap: 'inline',
     chunkSizeWarningLimit: 700,
     rollupOptions: {
       output: {
